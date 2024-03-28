d0ba41fabdd6051256240721bc283cd61e739a97
Martin Kučera <kutschera.biz@gmail.com>
2024-03-28 23:09:21 +0100

    v1

--- setup/app.py
+++ setup/app.py
@@ -2,23 +2,26 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv()
+# Načte data
+df = pd.read_csv('https://raw.githubusercontent.com/RDeconomist/observatory/main/Bitcoin%20Price.csv')
 
+# Inicializuje aplikaci
 app = Dash(__name__)
 
+# Definuje layout aplikace
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    html.H1(children='BTC', style={'textAlign':'center'}),
     dcc.Graph(id='graph-content')
 ])
 
+# Definuje callback pro aktualizaci grafu
 @callback(
     Output('graph-content', 'figure'),
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+    dff = df[df.Currency==value]
+    return px.line(dff, x='Date', y='24h High (USD)')
 
 if __name__ == '__main__':
     app.run(debug=True)
