3e299d83e4d42433af26df00dbc77702ac60f33e
Martin Kučera <kutschera.biz@gmail.com>
2024-03-28 22:40:43 +0100

    Update app.py

--- setup/app.py
+++ setup/app.py
@@ -2,7 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('"C:\Users\kutsc\Desktop\bitcoin.csv".csv')
+df = pd.read_csv()
 
 app = Dash(__name__)
 
