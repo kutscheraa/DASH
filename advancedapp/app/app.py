18545bfdeccd5b3d205a42edd91c9a9bf806e4c4
jelinekjakub <jelinekjakub@icloud.com>
2024-04-01 01:30:41 +0200

    update

--- advancedapp/app/app.py
+++ advancedapp/app/app.py
@@ -1,15 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-# Pass the required route to the decorator. 
-@app.route("/hello") 
-def hello(): 
-    return "Hello, Welcome to Geeks"
     
 @app.route("/") 
 def index(): 
-    return "Homepage of GeeksFor"
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
