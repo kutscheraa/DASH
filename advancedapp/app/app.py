07bed8fcf8d09312f3c6b3ed045ea70c81067170
jelinekjakub <jelinekjakub@icloud.com>
2024-04-01 23:31:43 +0200

    order update

--- advancedapp/app/app.py
+++ advancedapp/app/app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
     
-@app.route("/") 
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
 def index(): 
     return render_template('index.html', title="Home")
+
+@app.route("/order") 
+def order(): 
+    return render_template('order.html', title="Order")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
