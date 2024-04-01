6d2316897dc0b152aa3c50048c04cf0b22906478
jelinekjakub <jelinekjakub@icloud.com>
2024-04-02 01:07:06 +0200

    template decomposition, login template and error handling

--- advancedapp/app/app.py
+++ advancedapp/app/app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-    
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', error = error), 404
+
+@app.errorhandler(405)
+def page_not_found(error):
+    return render_template('error.html', error = error), 405
+
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
@@ -11,6 +20,12 @@ def index():
 @app.route("/order") 
 def order(): 
     return render_template('order.html', title="Order")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    return 'To be implemented'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
