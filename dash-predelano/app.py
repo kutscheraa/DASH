1d4f23afa0217907b80c63bdd3d0a6e15f95eb01
Martin Kučera <kutschera.biz@gmail.com>
2024-04-10 00:31:06 +0200

    Update app.py (removed seeders)

--- dash-predelano/app.py
+++ dash-predelano/app.py
@@ -12,10 +12,6 @@ app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP,
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 server = app.server
 
-# Seeding db
-#order_seeder.seed()
-#user_seeder.seed()
-
 ############################################################################################
 # Import shared components
 
